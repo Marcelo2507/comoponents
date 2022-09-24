60cd97dbfada68772edb9ee7d8b31eff51a86c17
Marcelo2507 <marceloluigi12.p8@gmail.com>
2022-09-24 01:07:32 +0000

    alterar o projeto para calular no click do botao

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
+import Dados from './components/Dados';
 
 export default function App() {
-  return (
+			const [nome, setNome] = useState("");
+			const [nomeParam, setNomeParam] = useState("");
+			const [idade,setIdade]= useState("");
+			const [idadeParam, setIdadeParam] = useState("");
+	
+		function gravar(){
+			setNomeParam(nome);
+			setIdadeParam(idade);
+		}
+			return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+		  <View style={styles.bloco}>
+				<Text style={styles.label}>Nome:</Text>
+			<TextInput
+				style={styles.input}
+				value={nome}
+				onChangeText={(value)=>setNome(value)}/>
+				
+		  </View>
+					
+		 <View style={styles.bloco}>			 		  		
+				<Text style={styles.label}>Ano Nasc.:</Text>
+			<TextInput
+				style={styles.input}
+				value={idade}
+				onChangeText={(value)=>setIdade(value)} 
+				/> 
+			  
+		  </View>
+					
+		  <View style={styles.bloco}>
+				<TouchableOpacity style={styles.botao} onPress={gravar}>
+					<Text style={styles.textBotao}>Mostrar</Text>
+				 </TouchableOpacity>
+		  </View>
+					
+		  <View style={styles.bloco}>
+			<Dados nome={nomeParam} idade={idadeParam}/>
+		  </View>
     </View>
   );
 }
@@ -13,8 +49,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
   },
+	bloco:{
+	    width:'80%',
+		marginTop:20,
+		marginLeft:'10%'
+	},
+	label:{
+	    fontSize:20
+	},
+	input:{		
+		borderWdth:2,
+		fontSize:20
+	},
+	botao:{		
+		backgroundColor:'#000'
+	},
+	textBotao:{
+		color:'#fff',
+		fontSize:20,
+		textAling:'center'
+	}
 });
